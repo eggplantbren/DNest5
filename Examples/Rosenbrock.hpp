a82b9e1d3232115f220c046c401c3b7016d41d53
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-08-04 10:33:08 +1200

    Rebase Rosenbrock on UniformModel

--- Examples/Rosenbrock.hpp
+++ Examples/Rosenbrock.hpp
@@ -1,55 +1,35 @@
 #ifndef DNest5_Rosenbrock_hpp
 #define DNest5_Rosenbrock_hpp
 
-#include <cstring>
-#include <DNest5/Misc.hpp>
-#include <DNest5/RNG.hpp>
-#include <vector>
+#include <DNest5/UniformModel.hpp>
 
 namespace DNest5
 {
 
-class Rosenbrock
+class Rosenbrock : public UniformModel<50>
 {
     private:
-
-        // Parameters
-        std::vector<double> xs;
 
     public:
 
         Rosenbrock(RNG& rng);
-        double perturb(RNG& rng);
+        void us_to_params();
         double log_likelihood() const;
-        std::vector<char> to_blob() const;
-        void from_blob(const std::vector<char>& vec);
 };
 
 /* Implementations follow */
 
 Rosenbrock::Rosenbrock(RNG& rng)
-:xs(50)
+:UniformModel(rng)
 {
-    for(double& x: xs)
-        x = -10.0 + 20.0*rng.rand();
+    us_to_params();
 }
 
-double Rosenbrock::perturb(RNG& rng)
+void Rosenbrock::us_to_params()
 {
-    int num = 1;
-    if(rng.rand() <= 0.5)
-        num = int(pow(xs.size(), rng.rand()));
-
-    for(int i=0; i<num; ++i)
-    {
-        int k = rng.rand_int(xs.size());
-        xs[k] += 20.0*rng.randh();
-        wrap(xs[k], -10.0, 10.0);
-    }
-
-    return 0.0;
+    for(size_t i=0; i<xs.size(); ++i)
+        xs[i] = -10.0 + 20.0*us[i];
 }
-
 
 double Rosenbrock::log_likelihood() const
 {
@@ -64,37 +44,6 @@ double Rosenbrock::log_likelihood() const
     logl *= 2;
 
     return logl;
-}
-
-std::vector<char> Rosenbrock::to_blob() const
-{
-    // Could just return xs, but writing this in full
-    // to show how it would be done if the parameter space were
-    // more complex
-    int chars_per_element = sizeof(double)/sizeof(char);
-    int chars_needed = xs.size()*chars_per_element;
-    std::vector<char> result(chars_needed);
-    int j = 0;
-    for(size_t i=0; i<xs.size(); ++i)
-    {
-        std::memcpy(&result[j], &xs[i], sizeof(double));
-        j += chars_per_element;
-    }
-    return result;
-}
-
-void Rosenbrock::from_blob(const std::vector<char>& vec)
-{
-    int chars_per_element = sizeof(double)/sizeof(char);
-    int chars_needed = xs.size()*chars_per_element;
-    assert(int(vec.size()) == chars_needed);
-
-    int j = 0;
-    for(size_t i=0; i<xs.size(); ++i)
-    {
-        std::memcpy(&xs[i], &vec[j], sizeof(double));
-        j += chars_per_element;
-    }
 }
 
 } // namespace
